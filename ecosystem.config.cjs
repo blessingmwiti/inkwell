0d115f0fb011a33dcfebe6e3e8ea5c1c9400f0c4
Blessing Mwiti <blessingmwiti@gmail.com>
2026-06-19 16:18:52 +0300

    path issue sorted

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
         PM2_SERVE_PATH: path.join(__dirname, "dist"),
         PM2_SERVE_PORT: process.env.PORT || 3000,
         PM2_SERVE_SPA: "true",
-        PM2_SERVE_HOMEPAGE: "index.html",
+        PM2_SERVE_HOMEPAGE: "/index.html",
       },
     },
   ],
